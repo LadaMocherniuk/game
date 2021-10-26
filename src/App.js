62dc8881c54c259ede1e3690e2aafa3c10be13d8
LadaMocherniuk <ladamochernyuk@gmail.com>
2021-10-26 19:55:50 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+        areas: Array(9).fill(null),
+        step: 0,
+    }
+    this.combination = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    };
+
+    
+
+clickHandler = e => {
+    let data = e.target.getAttribute('data');
+    let zone = this.state.areas;
+
+    if(zone[data] === null){
+        zone[data] = (this.state.step % 2 === 0 )? 'X' : 'O';
+        this.setState({step: this.state.step + 1});
+        this.setState({areas: zone});
+    }else{
+        return;
+    }
+    this.isWinner();
+    
+}
+ isWinner = () => {
+    let win = (this.state.step % 2 === 0 )? 'X' : 'O';
+    for ( let i = 0; i < this.combination.length; i++){
+        let line = this.combination[i];
+        if(this.state.areas[line[0]] === win && 
+            this.state.areas[line[1]] === win &&
+            this.state.areas[line[2]] === win ){
+                alert(win + '  player win 🏆');
+            setTimeout(() =>{
+                this.setState({areas: Array(9).fill(null)});
+                this.setState({step: 0});
+            }, 4000);
+        }
+
+    }
+
 }
 
+render(){
+    return(
+        <div className="block">
+            <div className="grid" onClick={this.clickHandler} data="0">
+        {this.state.areas[0]}
+            </div>
+            <div className="grid" onClick={this.clickHandler} data="1">
+        {this.state.areas[1]}    
+            </div>
+            <div className="grid" onClick={this.clickHandler} data="2">
+        {this.state.areas[2]}        
+            </div>
+            <div className="grid" onClick={this.clickHandler} data="3">
+        {this.state.areas[3]}        
+            </div>
+            <div className="grid" onClick={this.clickHandler} data="4">
+        {this.state.areas[4]}    
+            </div>
+            <div className="grid" onClick={this.clickHandler} data="5">
+        {this.state.areas[5]}    
+            </div>
+            <div className="grid" onClick={this.clickHandler} data="6">
+        {this.state.areas[6]}  
+            </div> 
+            <div className="grid" onClick={this.clickHandler} data="7">
+        {this.state.areas[7]}
+            </div>   
+            <div className="grid" onClick={this.clickHandler} data="8">
+        {this.state.areas[8]}    
+            </div>
+        </div>
+    )
+};
+};
 export default App;
